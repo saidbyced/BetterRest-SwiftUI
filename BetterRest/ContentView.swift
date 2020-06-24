653d8c58333980d28de7806d125b84bc0025203e
Chris Eadie <chris.eadie@outlook.com>
2020-06-24 19:43:34 +0100

    Set up basic View

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -9,9 +9,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var wakeUp = Date()
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                Text("Desired amount of sleep")
+                Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+                    Text("\(sleepAmount, specifier: "%g") hours")
+                }
+                Text("Daily coffee intake")
+                Stepper(value: $coffeeAmount, in: 1...20) {
+                    Text("\(coffeeAmount) \(coffeeAmount == 1 ? "cup" : "cups")")
+                }
+            }
+            .navigationBarTitle("BetterRest")
+            .navigationBarItems(trailing:
+                Button(action: calculateBedtime) {
+                    Text("Calculate")
+                }
+            )
+        }
     }
+    
+    func calculateBedtime() {
+        
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
